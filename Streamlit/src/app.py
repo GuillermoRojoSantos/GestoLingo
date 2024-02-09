df38b80088c9db09406f5f635c51195a40ffb42b
Wiltonfen <wiltonfen@gmail.com>
2024-02-09 17:39:09 +0100

    Probemos con tensorflow

--- Streamlit/src/app.py
+++ Streamlit/src/app.py
@@ -1,18 +1,17 @@
 import streamlit as st
-from opencv import cv2
-import numpy as np
+import tensorflow as tf
 
 img_file_buffer = st.camera_input("Take a picture")
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
+    # To read image file buffer as a 3D uint8 tensor with TensorFlow:
     bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    img_tensor = tf.io.decode_image(bytes_data, channels=3)
 
-    # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
+    # Check the type of img_tensor:
+    # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
+    st.write(type(img_tensor))
 
-    # Check the shape of cv2_img:
+    # Check the shape of img_tensor:
     # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
+    st.write(img_tensor.shape)
