761e618d7a903c0366cd9ecc33b3941b6d7d9f02
JADA <jada1998velez@gmail.com>
2024-02-26 21:17:21 +0100

    primeros cambios con el streamlit

--- src/streamlit/streamlit_main.py
+++ src/streamlit/streamlit_main.py
@@ -1,5 +1,21 @@
 import streamlit as st
-from streamlit.components.v1 import html
+import streamlit.components.v1 as com
+
+## Configuración
+st.set_page_config(
+    page_title="Gestolingo",
+    layout="wide"
+)
+
+# Using object notation
+add_selectbox = st.sidebar.image('images/logo1.png')
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 my_js = """
 const videoElement = document.getElementsByClassName('input_video')[0];
@@ -27,8 +43,8 @@ const videoElement = document.getElementsByClassName('input_video')[0];
     hands.setOptions({
       maxNumHands: 2,
       modelComplexity: 1,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
+      minDetectionConfidence: 0.7,
+      minTrackingConfidence: 0.9
     });
     hands.onResults(onResults);
     
@@ -41,8 +57,58 @@ const videoElement = document.getElementsByClassName('input_video')[0];
     });
     camera.start();
 """
-st.title("Javascript example")
-html(f'''
+# st.title("Javascript example")
+
+
+
+header = '''
+    <header>
+        <div id="logo-container">
+            <img id="logo" src="images/logo1.png" alt="Logo de Gestolingo">
+        </div>
+        <div id="app-name">Gestolingo</div>
+    </header>
+'''
+
+body = '''
+
+        <h1>¡Bienvenido a Gestolingo!</h1>
+        <p> Explora la revolución en la comunicación inclusiva a través de nuestro traductor de lenguaje de signos inteligente.</p>
+
+        <div class="features">
+            <div class="feature">
+                <h2>Traducción Instantánea</h2>
+                <p>Convierte gestos y expresiones en texto comprensible al instante.</p>
+            </div>
+            <div class="feature">
+                <h2>Interfaz Amigable</h2>
+                <p>Diseñada pensando en la facilidad de uso para usuarios de todas las edades.</p>
+            </div>
+            <div class="feature">
+                <h2>Accesibilidad Total</h2>
+                <p>Disponible en cualquier momento y lugar a través de la web, sin necesidad de descargas.</p>
+            </div>
+        </div>
+
+
+    '''
+    
+footer = '''
+    <footer>
+        &copy; 2024 Gestolingo - Traductor de Lenguaje de Signos
+    </footer>
+
+
+
+         '''
+with open('./style.css') as css:
+        st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html = True)
+st.markdown(header, unsafe_allow_html=True)
+st.markdown(body, unsafe_allow_html=True)
+st.markdown(footer, unsafe_allow_html=True)
+
+
+com.html(f'''
 <!DOCTYPE html>
 <html>
 <head>
@@ -58,10 +124,9 @@ html(f'''
 
 <body>
   <div class="container">
-    <video class="input_video"></video>
-    <canvas class="output_canvas" width="1280px" height="720px"></canvas>
+    <video class="input_video" style="width: 100%; height: 100%;"></video>
+    <canvas class="output_canvas" width="1280px" height="720px" style="width: 100%; height: 100%;"></canvas>
   </div>
 </body>
 </html>
-''')
-
+''', height = 10000)
