901b19102be9ff1d383cdcb1b3b44eadd0c8242d
Guillermo Rojo <wthereddev@gmail.com>
2024-02-27 11:56:04 +0100

    extract_hands.py now correctly detects hands commented functions on Utils.py because they aren't needed at the moment

--- src/extract_hands.py
+++ src/extract_hands.py
@@ -1,24 +1,52 @@
 import mediapipe as mp
 import cv2
-from mediapipe.python.solutions.holistic import Holistic
-
 import Utils as ut
 
-mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
+# Camera capture object's initialization
+cap = cv2.VideoCapture(0)
 
-with Holistic() as hm:
-    cap = cv2.VideoCapture(0)
+with mp.solutions.hands.Hands(
+    # Parametro para especificar la complejidad del modelo usado en la detección de las manos
+    model_complexity=0,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.9
+) as mp_hands:
     while True:
+        # Read Camera
         _, frame = cap.read()
-        hd_frame, results = ut.holistic_detection(frame, hm)
 
-        # if results.left_hand_landmarks or results.right_hand_landmarks:
+        # Predict hand landmarks
+        frame.flags.writeable = False
+        # Conversion the Frame from BGR to RGB
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        results = mp_hands.process(frame)
 
-        ut.draw_hand_keypoints(hd_frame,results)
-        cv2.imshow("GestoLingo", hd_frame)
+        # Draw the annotations on the image
+        frame.flags.writeable = True
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-        if cv2.waitKey(1) == 27:
+        x = 0
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+
+                if x<1:
+                    print(hand_landmarks)
+                    x = x + 1
+                mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp.solutions.hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style()
+                )
+
+        # Show the image, but flip horizontally it to have a selfie-view display
+        cv2.imshow('GestoLingo', frame, 1)
+        # If 'Esc'  is pressed, close the app
+        if cv2.waitKey(5) == 27:
             break
 
 cap.release()
