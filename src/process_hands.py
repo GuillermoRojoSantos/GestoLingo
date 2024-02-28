794361bbca0f266b9b96838b284682533fbab9f6
Guillermo Rojo <wthereddev@gmail.com>
2024-02-28 22:58:02 +0100

    corrected the way process_hands.py handles frame notation when making the .h5 file

--- src/process_hands.py
+++ src/process_hands.py
@@ -5,26 +5,26 @@ import re
 import pandas as pd
 import numpy as np
 
-
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+f_counter = 0
 
 if not os.path.exists("../data/dataFrames/"):
     os.mkdir("../data/dataFrames/")
 
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.2,
-    min_tracking_confidence=0.9
+        model_complexity=0,
+        min_detection_confidence=0.2,
+        min_tracking_confidence=0.9
 ) as hands:
     for lista in os.listdir("../data/words/"):
-        df = pd.DataFrame(columns=["n_sample","frame","keypoints"])
+        df = pd.DataFrame(columns=["n_sample", "frame", "keypoints"])
         for sample in os.listdir(f"../data/words/{lista}/"):
             for image in os.listdir(f"../data/words/{lista}/{sample}/"):
                 frame = cv2.imread(f"../data/words/{lista}/{sample}/{image}")
 
-                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 result = hands.process(frame)
                 left_hand = []
                 right_hand = []
@@ -40,19 +40,26 @@ with mp_hands.Hands(
 
                         else:
                             # Mano derecha
-                            right_hand = np.array([[res.x,res.y,res.z] for res in hand_landmark.landmark]).flatten()
+                            right_hand = np.array([[res.x, res.y, res.z] for res in hand_landmark.landmark]).flatten()
 
+                    # si alguna mano está vacia, rellenar con 0
+                    if len(left_hand) == 0:
+                        left_hand = np.zeros(21 * 3)
+                    elif len(right_hand) == 0:
+                        right_hand = np.zeros((21 * 3))
 
-                    #si alguna mano está vacia, rellenar con 0
-                    if len(left_hand)==0:
-                        left_hand = np.zeros(21*3)
-                    elif len(right_hand)==0:
-                        right_hand = np.zeros((21*3))
-
-                    res_hand_landmarks = np.concatenate([left_hand,right_hand])
+                    res_hand_landmarks = np.concatenate([left_hand, right_hand])
                     # Agregar filas al DataFrame
-                    df.loc[len(df.index)] = {"n_sample":int(re.findall("\d+",sample)[0]),"frame":int(re.findall("\d+",image)[0])+1,"keypoints":[res_hand_landmarks]}
+
+                    df.loc[len(df.index)] = {"n_sample": int(re.findall("\d+", sample)[0]),
+                                             "frame": int(re.findall("\d+", image)[0]) + 1,
+                                             "keypoints": res_hand_landmarks}
+                    df.loc[len(df.index)] = {"n_sample": int(re.findall("\d+", sample)[0]),
+                                             "frame": f_counter,
+                                             "keypoints": res_hand_landmarks}
+                    f_counter += 1
 
                     print(df.head())
+            f_counter = 0
 
-df.to_hdf(f"../data/dataFrames/{lista}.h5",key="data",mode="w")
+df.to_hdf(f"../data/dataFrames/{lista}.h5", key="data", mode="w")
